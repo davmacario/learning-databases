73cb4f31833626e02352122e33368ea4f0ede63c
davmacario <dav.macario@gmail.com>
2024-04-14 22:41:55 -0500

    Add first SQL query

---
+++ create_new_expenses.sql
@@ -0,0 +1,10 @@
+CREATE TABLE expenses (
+    id SERIAL PRIMARY KEY,
+    description VARCHAR,
+    amount NUMERIC,
+    date DATE
+);
+
+INSERT INTO expenses (description, amount, date) VALUES ("Groceries", 50.00, "2024-04-14");
+
+SELECT * FROM expenses;
